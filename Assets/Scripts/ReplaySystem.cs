4658ff6d4f4b8f775eccce5395550980fbcf4842
DESKTOP-IIM4DVV\CAP <allenrader2002@gmail.com>
2018-05-15 10:05:04 -0500

    Creating A Replay System

---
+++ Assets/Scripts/ReplaySystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+	private const int bufferFrames = 100;
+	private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+	private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Record ();
+	}
+
+	void Playback  () {
+		rigidbody.isKinematic = true;
+		int frame = Time.frameCount % bufferFrames;
+		print ("Reading frame " + frame);
+		transform.position = keyFrames [frame].position;
+		transform.rotation = keyFrames [frame].rotation;
+	}
+
+	void Record () {
+		rigidbody.isKinematic = false;
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		print ("Writing frame " + frame);
+		keyFrames [frame] = new MyKeyFrame (time, transform.position, transform.rotation);
+	}
+}
+/// <summary>
+/// A structure for storing time, position, and rotation
+/// </summary>
+public struct MyKeyFrame {
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public MyKeyFrame (float aTime, Vector3 aPosition, Quaternion aRotation) {
+		frameTime = aTime;
+		position = aPosition;
+		rotation = aRotation;
+	}
+}
