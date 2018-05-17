6ebca4d696fb567a6e8dee9e4c090045c37bc793
DESKTOP-IIM4DVV\CAP <allenrader2002@gmail.com>
2018-05-17 11:42:19 -0500

    Reading From 2nd Stick

---
+++ Assets/Scripts/CameraPan.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CameraPan : MonoBehaviour {
+
+	private GameObject player;
+	//private Vector3 cameraOffset;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		//cameraOffset = new Vector3 (-10, 2, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LateUpdate () {
+		transform.LookAt (player.transform);
+		//transform.position = player.transform.position + cameraOffset;
+	}
+}
